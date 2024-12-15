7dfe6a8fe67fdb7e7407f045ec5a0367b86e5932
Tony Hsieh <hsieh2888-github@yahoo.com>
2024-12-14 19:25:16 -0800

    Adding storage

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,3 +1,12 @@
 export const bucket = new sst.aws.Bucket("Uploads", {
   access: "public"
 });
+
+// Create the DynamoDB table
+export const table = new sst.aws.Dynamo("Notes", {
+  fields: {
+    userId: "string",
+    noteId: "string",
+  },
+  primaryIndex: { hashKey: "userId", rangeKey: "noteId" },
+});
